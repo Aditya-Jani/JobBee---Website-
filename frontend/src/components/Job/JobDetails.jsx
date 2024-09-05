2cf29c9874224841e3d11e9ea168b7a10d824198
Aditya Jani <101978532+Aditya-Jani@users.noreply.github.com>
2024-09-06 00:38:59 +0530

    Update JobDetails.jsx

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../../main";
+import {BASE_URL} from "../../urls";
+
 const JobDetails = () => {
   const { id } = useParams();
   const [job, setJob] = useState({});
@@ -12,7 +14,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`${BASE_URL}/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
