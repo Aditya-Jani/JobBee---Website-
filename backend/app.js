8e72e7b6418ea79e19c0a627b4e52391eb0b620a
Aditya Jani <101978532+Aditya-Jani@users.noreply.github.com>
2024-09-06 01:23:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,40 +9,24 @@ import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
-
+const app = express();
 config({ path: "./config/config.env" });
 
 // const cors = require('cors');
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     method: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
-const express = require("express");
-const app = express();
-// Set middleware of CORS 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL || "https://jobzee-website-18.onrender.com"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://jobzee-website-18.onrender.com'); 
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 app.use(cookieParser());
 app.use(express.json());
