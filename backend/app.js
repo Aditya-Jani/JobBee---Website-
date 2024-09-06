be7c018fd8161f1cdd943a25bbad74adfaa486f4
Aditya Jani <101978532+Aditya-Jani@users.noreply.github.com>
2024-09-06 11:45:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,21 +13,21 @@ const app = express();
 config({ path: "./config/config.env" });
 
 // const cors = require('cors');
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your frontend domain
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true'); // This is crucial!
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your frontend domain
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true'); // This is crucial!
+//   next();
+// });
 
 app.use(cookieParser());
 app.use(express.json());
